78087a913bd40c4c3b6550a846cc1c8287abbb79
Hannah Stampke <hannah.stampke@gmail.com>
2022-07-31 23:30:12 +0930

    added code to install.js, htmlRoutes.js and server.js

---
+++ Client/src/js/install.js
@@ -0,0 +1,7 @@
+const btnInstall = document.getElementById('btnInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {});
+
+btnInstall.addEventListener('click', async () => {});
+
+window.addEventListener('appinstalled', (event) => {});
